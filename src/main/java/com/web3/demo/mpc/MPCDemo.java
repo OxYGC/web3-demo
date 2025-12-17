bbb3439b0fade3c78a9d0d6676ffb6e637fe20a1
OxYGC <keygenee@gmail.com>
2025-12-17 23:16:30 +0800

    MPC Demo

--- src/main/java/com/web3/demo/mpc/MPCDemo.java
+++ src/main/java/com/web3/demo/mpc/MPCDemo.java
@@ -49,4 +49,17 @@ public class MPCDemo {
             System.out.println("❌ 不一致");
         }
     }
+
+
+    /**
+     * 打印输出(x,y,z坐标是“内部状态”，对协议安全性没有直接作用，但对性能至关重要, 坐标是“内部状态”，对协议安全性没有直接作用，但对性能至关重要,Z 坐标记录了比例关系，最后统一归一化得到标准公钥)
+     * Aggregated Public Key:
+     * (908faa6e3c001e586a3be84751a2c5c43121b627add3cb19d881c24f62de027d,151d375c499d9067950792897362174f67bbda08facaa794765421cabd5ad673,1)
+     * Aggregated Private Key:
+     * 8a9c521a84732d500fcba714fcd811c40c214ec4bae22f79718e1a12b3c72dd5
+     * Derived Public Key from Aggregated Private Key:
+     * (908faa6e3c001e586a3be84751a2c5c43121b627add3cb19d881c24f62de027d,151d375c499d9067950792897362174f67bbda08facaa794765421cabd5ad673,1)
+     * ✅ 聚合公钥与聚合私钥推算的公钥一致
+     */
+
 }
